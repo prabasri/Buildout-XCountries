985ba5cd3f5b83cdac4248102f7e8f814a78fe29
Praba sri <prabasri1998@gmail.com>
2024-03-24 20:35:32 +0530

    buildout xCountries

---
+++ src/components/card/card.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import styles from "./card.module.css";
+
+const Card = () => {
+ const [countries, setCountries] = useState([]);
+
+const fetchCountries = async() => {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const result = await response.json();
+    setCountries(result);
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+useEffect(() => {
+  fetchCountries();
+}, []);
+ 
+console.log(countries);
+
+return (
+  <div className={styles.grid}>
+    {
+      countries.map((country) => (
+        <div className={styles.card}>
+          <img src={country.flags.png} alt={country.name.common} width={100} height={80}/>
+          <p>{country.name.common}</p>
+        </div> 
+      ))
+    }
+  </div>
+)
+} 
+
+export default Card;
